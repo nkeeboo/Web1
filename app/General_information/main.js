a38f475c3cdea742016f9a4ce9de3cdc4a50b27c
bichsnhuw <87610295+Ngtbnhu@users.noreply.github.com>
2021-11-21 22:24:28 +0700

    BN nop dl

---
+++ app/General_information/main.js
@@ -0,0 +1,7 @@
+document.getElementById("idfname").innerHTML = fname
+document.getElementById("idlname").innerHTML = lname
+document.getElementById("idaname").innerHTML = aname
+document.getElementById("idemail").innerHTML = mail
+document.getElementById("iddob").innerHTML = dob
+document.getElementById("idphonenb").innerHTML = phonenb
+document.getElementById("idpd").innerHTML = pd
