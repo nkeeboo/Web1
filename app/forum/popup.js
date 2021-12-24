ee59f7995e29942369d7b89ff942bd12749cb8e8
DiemQuynhff <DiemQuynhff>
2021-12-24 15:10:32 +0700

    responsive forum

---
+++ app/forum/popup.js
@@ -0,0 +1,30 @@
+// Get the popup
+var popup = document.getElementById("myPopup");
+
+// Get the button that opens the popup
+var btn = document.getElementById("myCreateTopicBtn");
+var btn = document.getElementsByClassName("reply__btn");
+
+// Get the <span> element that closes the popup
+var span = document.getElementsByClassName("close")[0];
+var cancel = document.getElementsByClassName("cancel__btn")[0];
+
+// When the user clicks the button, open the popup
+btn.onclick = function() {
+    popup.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the popup
+span.onclick = function() {
+    popup.style.display = "none";
+}
+cancel.onclick = function() {
+    popup.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the popup, close it
+window.onclick = function(event) {
+    if (event.target == popup) {
+        popup.style.display = "none";
+    }
+}
